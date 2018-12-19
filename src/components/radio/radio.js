27a2ea141b1a704e2a4cd29cff6c2df2692612e6
dengyingyi2 <dengyingyi@jd.com>
2018-12-19 19:23:36 +0800

    dev

--- src/components/radio/radio.js
+++ src/components/radio/radio.js
@@ -1,1 +1,23 @@
-alert(1);
+//jui-radio
+$(function () {
+    console.log($('.jui-radio').length);
+    // var className = $('.jui-radio').prop('class');
+    // console.log(className);
+    $('.jui-radio').each(function (index, element) {
+        console.log('element', element);
+        var className = $(element).prop('class');
+        //TODO 处理 className；
+        console.log(className);
+        var radio = '<label class="jui-radio-wrapper ' + className + '">' +
+            '<span class="jui-radio-input">' +
+            '<input type="radio">' +
+            '<span></span>' +
+            '</span>' +
+            '<span class="jui-radio-label">' +
+            '2222' +
+            '</span>' +
+            '</label>'
+        $(element).after(radio);
+        $(element).remove();
+    })
+})
