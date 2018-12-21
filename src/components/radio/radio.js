cb611b0ba3b43385bd744034ebbe1c4d401f5a8c
dengyingyi2 <dengyingyi@jd.com>
2018-12-21 09:51:18 +0800

    dev

--- src/components/radio/radio.js
+++ src/components/radio/radio.js
@@ -5,7 +5,7 @@ $(function () {
     // var className = $('.jui-radio').prop('class');
     // console.log(className);
     $('.jui-radio').each(function (index, element) {
-        console.log('element', element);
+        // console.log('element', element);
         var className = $(element).prop('class');
         var name = $(element).prop('name');
         //TODO 处理 className；
@@ -30,17 +30,28 @@ $(function () {
             '备选项<!---->' +
             '</span>' +
             '</label>';
-       
-        $(element).after(html);
+        var dom = $(html)[0];
+        $(element).after(dom);
+        console.log('tt', dom);
         $(element).remove();
+        addEvent(dom)
     })
+    function addEvent(dom){
+        dom.change = function(){
+            // alert(8);            
+        }
+    }
 
-    //事件
-    $('input[type="radio"]').on('change', function(){
-        console.log(this);
-        alert(12);
-        console.log($(this).prop('checked'));
+    $('body').on('click', '.jui-radio-wrapper', function(){
+        console.log(this.change());
     })
+    
+    //事件
+    // $('input[type="radio"]').on('change', function(){
+    //     console.log(this);
+    //     alert(12);
+    //     console.log($(this).prop('checked'));
+    // })
 
     // $('.jui-radio-style').on('click', function(){
     //     alert(2);
