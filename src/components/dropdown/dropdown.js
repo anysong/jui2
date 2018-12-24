a90e93b2dfb79727e4683a7ec103458c02fd6705
dengyingyi2 <dengyingyi@jd.com>
2018-12-24 20:38:14 +0800

    dev

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,81 +1,35 @@
 //jui-dropdown
 $(function () {
     $('.jui-dropdown').each(function (index, element) {
-       
-    })
-    //绑定自定义事件
-    function addEvent(dom) {
-        $(dom).on('change', function () {
-            this.onzrchange? this.onzrchange():'';
-        })
-        dom.onzrchange = function (option) {
-            var $input = $(this),
-                original = this,
-                $parent = $input.parent(),
-                $label = $input.closest('label'),
-                inputName = $input.prop('name');
-            var opt = option || {};
+        var $element = $(element),
+            event = $element.attr('data-event');
 
-            for(var name in opt){
-                if(name === 'checked') $input.prop('checked', opt[name]);
-                if(name === 'disabled') $input.prop('disabled', opt[name]);
-                if(name === 'value') {
-                    if(opt[name].length == 0){
-                        $parent.siblings('.zr-radio-label').css('display','none');
-                    }else {
-                        $parent.siblings('.zr-radio-label').css('display','inline').html(opt[name]);
-                    }
-                };
-            }
-            if(opt.beforeFn) opt.beforeFn();
-            //选中
-            if ($input.prop('checked')) {
-                $parent.addClass('jui-radio-checked');
-                $label.addClass('jui-radio-wrapper-checked');
-                //其他项目取消选中
-                var aInput = $('input[name=' + inputName + ']');
-                aInput.each(function (i, element) {
-                    if (original !== element) {
-                        element.onzrchange? element.onzrchange(): '';
-                    }
-                })
-            }else {
-                $parent.removeClass('jui-radio-checked');
-                $label.removeClass('jui-radio-wrapper-checked');
-            }
-            //禁用
-            if ($input.prop('disabled')) {
-                $parent.addClass('jui-radio-disabled');
-                $label.addClass('jui-radio-wrapper-disabled');
-            }else {
-                $parent.removeClass('jui-radio-disabled');
-                $label.removeClass('jui-radio-wrapper-disabled');
-            }
-
-            if(opt.afterFn) opt.afterFn();
+        //点击
+        if (event === 'click') {
+            $element.on('click', function (ev) {
+                ev.stopPropagation();
+                $element.hasClass('open') ? $element.removeClass('open') : $element.addClass('open');
+            })
+        }else {
+            $element.on('mouseenter', function (ev) {
+                ev.stopPropagation();
+                $element.addClass('open');
+            })
+            $element.on('mouseleave', function (ev) {
+                ev.stopPropagation();
+                $element.removeClass('open');
+            })
         }
-    }
-
-    $('.js-select').on('click', function () {
-        console.log($('#id1')[0])
-        //选中
-        $('#id1')[0].onzrchange({
-            checked: true,
-            disabled: true,
-            value: 'xx',
-            beforeFn: function () {
-                console.log(1);
-            },
-            afterFn: function(){
-                console.log(2);
-            }
+        //组织冒泡
+        $element.on('click','.jui-dropdown-menu',function(ev){
+            ev.stopPropagation();
         })
     })
-    //监听
-    $(document).on("DOMNodeInserted", function(){
-        console.log('is');
-    })
-    $(document).on("DOMNodeRemoved", function(){
-        console.log('rm');
+     //全局
+    $(document).on('click', function(){
+        $('.jui-dropdown').each(function (index, element) {
+            var $element = $(element);
+            $element.removeClass('open');
+        })
     })
 })
