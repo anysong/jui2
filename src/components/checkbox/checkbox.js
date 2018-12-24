638be098e42da8d38e57b031bd7fe641ef2e2d50
dengyingyi2 <dengyingyi@jd.com>
2018-12-24 09:37:42 +0800

    dev

--- src/components/checkbox/checkbox.js
+++ src/components/checkbox/checkbox.js
@@ -1,60 +1,101 @@
-
 //jui-checkbox
+
 $(function () {
-    console.log($('.jui-checkbox').length);
-    // var className = $('.jui-checkbox').prop('class');
-    // console.log(className);
     $('.jui-checkbox').each(function (index, element) {
-        // console.log('element', element);
-        var className = $(element).prop('class');
-        var name = $(element).prop('name');
-        //TODO 处理 className；
-        var _arr = className.split(' ');
-        var _list = [];
-        var juiStyle = '',
-            adminStyle = '';
-        for (var i = 0; i < _arr.length; i++) {
-            if(_arr[i].indexOf('jui-') != -1){
-                juiStyle += ' ' + _arr[i];
-            }else {
-                adminStyle += ' ' + _arr[i];
-            }
-        }
-        var newclass = _list.join(' ');
-        var html = '<label class="jui-checkbox-wrapper ' + adminStyle + '">' +
-            '<span class="jui-checkbox-style' + juiStyle + '">' +
+        var $original = $(element);
+        var _class = $original.prop('class'),
+            _name = $original.prop('name'),
+            _checked = $original.prop('checked'),
+            _disabled = $original.prop('disabled'),
+            _value = $original.prop('value'),
+            _id = $original.prop('id');
+        
+        var html = '<label class="jui-checkbox-wrapper">' +
+            '<span class="jui-checkbox-clone">' +
             '<span class="jui-checkbox-inner"></span>' +
-            '<input type="checkbox" class="jui-checkbox-original" name="' + name + '">' +
+            '<input type="checkbox" class="' + _class + '">' + 
             '</span>' +
-            '<span class="jui-checkbox-label">' +
-            '备选项<!---->' +
+            '<span class="jui-checkbox-label">' + _value +
             '</span>' +
             '</label>';
-        var dom = $(html)[0];
-        $(element).after(dom);
-        console.log('tt', dom);
-        $(element).remove();
-        addEvent(dom)
+        var $dom = $(html),
+            $input = $dom.find('input');
+        //暂时不添加class
+        $input.prop('checked', _checked); //checked
+        $input.prop('disabled', _disabled); //disabled
+        $input.attr('name', _name); //name
+        $input.attr('id', _id); //id
+        $input.addClass('jui-checkbox-original');
+        $input.removeClass('jui-checkbox');
+        //已选中
+        if(_checked){
+            $input.parent().addClass('jui-checkbox-checked');
+            $input.closest('label').addClass('jui-checkbox-wrapper-checked');
+        }
+        //已禁用
+        if(_disabled){
+            $input.parent().addClass('jui-checkbox-disabled');
+            $input.closest('label').addClass('jui-checkbox-wrapper-disabled');
+        }
+        
+        var input = $input[0];
+        addEvent(input) //绑定事件
+        //
+        $original.after($dom);
+        $original.remove();
     })
-    function addEvent(dom){
-        dom.change = function(){
-            // alert(8);            
+    //绑定自定义事件
+    function addEvent(dom) {
+        dom.onzrchange = function (option) {
+            var $input = $(this),
+                original = this,
+                $parent = $input.parent(),
+                inputName = $input.prop('name');
+            var opt = option || {};
+
+            for(var name in opt){
+                if(name === 'checked') $input.prop('checked', opt[name]);
+                if(name === 'disabled') $input.prop('disabled', opt[name]);
+                if(name === 'value') $parent.siblings('span').html(opt[name]);
+            }
+            if(opt.beforeFn) opt.beforeFn();
+            //选中
+            if ($input.prop('checked')) {
+                $parent.addClass('jui-checkbox-checked');
+                $input.closest('label').addClass('jui-checkbox-wrapper-checked');
+            }else {
+                $parent.removeClass('jui-checkbox-checked');
+                $input.closest('label').removeClass('jui-checkbox-wrapper-checked');
+            }
+            //禁用
+            if ($input.prop('disabled')) {
+                $parent.addClass('jui-checkbox-disabled');
+                $input.closest('label').addClass('jui-checkbox-wrapper-disabled');
+            }else {
+                $parent.removeClass('jui-checkbox-disabled');
+                $input.closest('label').removeClass('jui-checkbox-wrapper-disabled');
+            }
+
+            if(opt.afterFn) opt.afterFn();
         }
     }
-
-    $('body').on('click', '.jui-checkbox-wrapper', function(){
-        console.log(this.change());
+    $('input[type="checkbox"]').on('change', function () {
+        this.onzrchange? this.onzrchange():''; //普通checkbox 没有onzrchange
     })
-    
-    //事件
-    // $('input[type="checkbox"]').on('change', function(){
-    //     console.log(this);
-    //     alert(12);
-    //     console.log($(this).prop('checked'));
-    // })
 
-    // $('.jui-checkbox-style').on('click', function(){
-    //     alert(2);
-    //     console.log($(this).children('input'));
-    // })
+    $('.js-select').on('click', function () {
+        console.log($('#id1')[0])
+        //选中
+        $('#id1')[0].onzrchange({
+            checked: true,
+            disabled: true,
+            value: 'xx',
+            beforeFn: function () {
+                console.log(1);
+            },
+            afterFn: function(){
+                console.log(2);
+            }
+        })
+    })
 })
