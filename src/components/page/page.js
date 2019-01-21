665ae4c57bd341f21a4f0242c2643744b7548a1c
dengyingyi2 <dengyingyi@jd.com>
2019-01-21 10:30:48 +0800

    dev

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -91,13 +91,111 @@ $(function () {
                 }
                 html += NEXT_M;
                 html += PAGE_M;
+                html += SIZE_M;
                 html += JUMP_M;
                 html += '</ul>';
                 $list.html(html);
+                initDropdown(); //初始化下拉
             };
             var parseDOM = function () {
 
-            }
+            };
+            var initDropdown = function () {
+                //初始化下拉组件
+                $element.find('.zr-dropdown').each(function (index, element) {
+                    var $element = $(element),
+                        $link = $element.children('.zr-dropdown-link'),
+                        $menu = $element.children('.zr-dropdown-menu');
+
+                    var type = $element.attr('data-type'),
+                        menuwidth = $element.attr('data-menuwidth'),
+                        menuheight = $element.attr('data-menuheight'),
+                        closeauto = $element.attr('data-close-auto'),
+                        closescroll = $element.attr('data-close-scroll'),
+                        closeresize = $element.attr('data-close-resize');
+
+                    //整体禁用
+                    if ($element.hasClass('zr-dropdown-disabled')) {
+                        return;
+                    }
+
+                    //事件委托 阻止冒泡
+                    $element.on('click', '.zr-dropdown-item-disabled', function (ev) {
+                        ev.stopPropagation();
+                        return false;
+                    })
+                    $element.on('click', '.zr-dropdown-item', function (ev) {
+                        ev.stopPropagation();
+                    })
+
+                    //自定义属性
+                    //展开框宽度限定
+                    if (menuwidth) {
+                        $menu.css('width', menuwidth);
+                    }
+                    if (menuheight) {
+                        $menu.css('height', menuheight);
+                    }
+                    //自动计算高度
+                    var autoHeight = function () {
+                        /**
+                         * 根据视窗距离屏幕上下距离,决定向上展示还是向下展示.
+                         * 上下同时不够,向下展开
+                         */
+                        var element_t = $element.offset().top,
+                            element_h = $element.height(),
+                            window_h = $(window).height(),
+                            scroll_h = $(document).scrollTop(),
+                            menu_pt_h = $menu.css('padding-top'),
+                            menu_pb_h = $menu.css('padding-bottom'),
+                            menu_h = $menu.height() + parseFloat(menu_pt_h) + parseFloat(menu_pb_h);
+                        //距离
+                        var bottom_h = window_h - (element_t - scroll_h) - element_h,
+                            top_h = window_h - bottom_h - element_h;
+
+                        if (menu_h < bottom_h) {
+                            $element.removeClass('zr-dropdown-up'); //down
+                        } else if (menu_h < top_h) {
+                            $element.addClass('zr-dropdown-up'); //up
+                        } else {
+                            $element.removeClass('zr-dropdown-up'); //down
+                        }
+                    }
+                    $(window).scroll(function () {
+                        if (!closeauto && !closescroll) autoHeight();
+                    })
+                    $(window).resize(function () {
+                        if (!closeauto && !closeresize) autoHeight();
+                    })
+                    //初始化
+                    if (!closeauto) autoHeight();
+                    //展开方式
+                    if (type === 'click') {
+                        $element.on('click', function (ev) {
+                            ev.stopPropagation();
+                            if (!closeauto) autoHeight();
+                            $element.hasClass('open') ? $element.removeClass('open') : $element.addClass('open');
+                        })
+                    } else {
+                        $element.on('mouseenter', function (ev) {
+                            ev.stopPropagation();
+                            if (!closeauto) autoHeight();
+                            $element.addClass('open');
+                        })
+                        $element.on('mouseleave', function (ev) {
+                            ev.stopPropagation();
+                            $element.removeClass('open');
+                        })
+                    }
+                })
+                //全局
+                $(document).on('click', function () {
+                    $('.zr-dropdown').each(function (index, element) {
+                        var $element = $(element);
+                        $element.removeClass('open');
+                    })
+                })
+            };
             var bindListener = function () {
                 $list.on('click', '.zr-pagination-item', function () {
                     var $li = $(this),
@@ -120,17 +218,22 @@ $(function () {
                 //下一页
                 NEXT_M = '<li class="zr-pagination-item"><a href="javascript:;">Next</a></li>';
                 //跳转
-                JUMP_M = '<li class="zr-pagination-options">'+
-                '<div class="zr-pagination-options-jump">跳转'+
-                '<input type="text">页'+
-                '</div>'+
-                '</li>';
+                JUMP_M = '<li class="zr-pagination-options">' +
+                    '<div class="zr-pagination-options-jump">跳转' +
+                    '<input type="text">页' +
+                    '</div>' +
+                    '</li>';
                 //总页数
-                PAGE_M = '<li class="zr-pagination-total-page">共'+ _page + '页</li>';
+                PAGE_M = '<li class="zr-pagination-total-page">共' + _page + '页</li>';
                 //总条数
-                TOTAL_M = '<li class="zr-pagination-total-text">共'+ _total + '条</li>';
+                TOTAL_M = '<li class="zr-pagination-total-text">共' + _total + '条</li>';
                 //每页条数
-                SIZE_M = '';
+                SIZE_M = '<div class="zr-dropdown zr-dropdown-btn" data-type="click">' +
+                    '<a class="zr-dropdown-link" href="javascript:;">Click1<i class="zr-icon-angle zr-icon-down"></i></a>' +
+                    '<div class="zr-dropdown-menu">' +
+                    '<li class="zr-dropdown-item"><a href="javascript:;">Action</a></li>' +
+                    '</div>' +
+                    '</div>';
                 //信息
                 INFO_M = '';
             }
